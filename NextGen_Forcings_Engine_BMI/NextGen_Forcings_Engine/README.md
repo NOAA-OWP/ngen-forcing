a01366a080c8c7714320c4f0c4b66b2bb3be0ee3
jduckerOWP <jason.ducker@noaa.gov>
2023-09-12 17:41:11 +0000

    Integrating NextGen Forcings Engine BMI onto GitHub from the RDPHCS Hera cluster

---
+++ NextGen_Forcings_Engine_BMI/NextGen_Forcings_Engine/README.md
@@ -0,0 +1,31 @@
+# Testing BMI Python modules.
+ - model.py: This file is the "NextGen Forcings Engine Driver" it takes inputs and gives an output
+ - bmi_model.py: This is the Basic Model Interface that talks with the model. This is the NextGen Forcings Engine BMI class.
+ - run_bmi_model.py: This is a file that mimics the framework, in the sense that it initializes the model with the BMI function. Then it runs the model with the BMI Update function, etc.
+ - run_bmi_unit_test.py: This is a file that runs each BMI unit test to make sure that the BMI is complete and functioning as expected.
+ - config.yml: This is a configuration file that the BMI reads to set inital_time (initial value of current_model_time) and all the required variables needed to drive the NextGen Forcings Engine.
+ - environment.yml: Environment file with the required Python libraries needed to run the model with BMI and the NextGen Forcings Engine. Create the environment with this command: `conda env create -f environment.yml`, then activate it with `conda activate bmi_test`
+ - slurm.job: Example Python submission script for a slurm job manager that is utilized to execute a MPI Python script (NextGen Forcings Engine) on a supercomputer.
+ - ./core/: The sub-directory containing all the support Python modules required to execute the NextGen Forcings Engine driver within model.py script.
+ - ./BMI_NextGen_Configs/: A sub-directory containing all the current config.yml BMI file setupts that will allow a a user to driver the NextGen BMI Forcings Engine for a gridded domain, a coastal-model unstructured mesh domain, and a NextGen hydrofabric (VPU 06) unstructured mesh domain. 
+ - ./NextGen_Domains/: A sub-directory that contains a sample domain geogrid file for a gridded domain, a coastal-model unstructured mesh domain, and a NextGen hydrofabric (VPU 06) unstructured mesh domain that can all be utitlized currently to regrid forcings for a Medium Range forecast simulation driver by GFS forcings.
+ - ./NWM_Params/: An empty sub-directory that will eventually contain supporting climatology files that will support implementing various bias calibration and downscaling technqius that are only associated with the original WRF-Hydro domain (aka NOAH-OWP Modular).
+ - ./Unit_Test_Output/: An sub-directory that is essentially the current scratch directory setup within each of the config.yml files to temporary place I/O commands and the log file of the progress of the NextGen Forcings Engine
+ - ./GFS_data/: A temporary sub-directory that contains a single GFS forecast cycle that is constrained to the start time of the BMI setup within each of the config.yml files.
+
+# About
+This is an implementation of a Python-based model that fulfills the Python language BMI interface and can be used in the Framework. It is intended to serve as a control for testing purposes, freeing the framework from dependency on any real-world model in order to test BMI related functionality.
+
+# Implementation Details
+
+## Test the complete BMI functionality
+`python run_bmi_unit_test.py`
+
+## Run the model
+`python run_bmi_model.py`
+
+## Sample output
+'model time', 'U2D_ELEMENT', 'V2D_ELEMENT', 'LWDOWN_ELEMENT','SWDOWN_ELEMENT','T2D_ELEMENT','Q2D_ELEMENT','PSFC_ELEMENT','RAINRATE_ELEMENT' 
+3600 -0.34978889998563434 1.9576997889275944 353.8881030867496 635.9115803301148 299.8541540030717 0.01014856288865672 101253.80092868667 0.0
+7200 -0.55664622087895 1.7230866051531413 351.70806444209256 446.07219911115914 299.7995647600879 0.01005461030680388 101259.55711493774 0.0
+10800 -0.2516353004284137 1.4639377367413153 347.3662126755073 221.91463406286712 299.4248073664293 0.01016205978429921 101303.53098399537 0.0
---
+++ NextGen_Forcings_Engine_BMI/NextGen_Forcings_Engine/core/layeringMod.py
@@ -0,0 +1,133 @@
+"""
+Layering module for implementing various layering schemes in the WRF-Hydro forcing engine.
+Future functionality may include blenidng, etc.
+"""
+import numpy as np
+from core import err_handler
+
+def layer_final_forcings(OutputObj,input_forcings,ConfigOptions,MpiConfig):
+    """
+    Function to perform basic layering of input forcings as they are processed. The logic
+    works as following:
+    1.) As the parent calling program loops through the forcings for each layer
+        for this timestep, forcings are placed onto the output grid by shear brute
+        replacement. However, this only occurs where valid data exists.
+        Supplemental precipitation will be layered in separately.
+    :param OutputObj:
+    :param input_forcings:
+    :param ConfigOptions:
+    :param MpiConfig:
+    :return:
+    """
+    # Loop through the 8 forcing products to layer in:
+    # 0.) U-Wind (m/s)
+    # 1.) V-Wind (m/s)
+    # 2.) Surface incoming longwave radiation flux (W/m^2)
+    # 3.) Precipitation rate (mm/s)
+    # 4.) 2-meter temperature (K)
+    # 5.) 2-meter specific humidity (kg/kg)
+    # 6.) Surface pressure (Pa)
+    # 7.) Surface incoming shortwave radiation flux (W/m^2)
+
+    for force_idx in range(0,8):
+        if force_idx in input_forcings.input_map_output:
+            if(ConfigOptions.grid_type == "gridded"):
+                outLayerCurrent = OutputObj.output_local[force_idx,:,:]
+                layerIn = input_forcings.final_forcings[force_idx,:,:]
+                indSet = np.where(layerIn != ConfigOptions.globalNdv)
+                outLayerCurrent[indSet] = layerIn[indSet]
+                OutputObj.output_local[force_idx, :, :] = outLayerCurrent
+                # Reset for next iteration and memory efficiency.
+                indSet = None
+            elif(ConfigOptions.grid_type == "unstructured"):
+                outLayerCurrent = OutputObj.output_local[force_idx,:]
+                layerIn = input_forcings.final_forcings[force_idx,:]
+                indSet = np.where(layerIn != ConfigOptions.globalNdv)
+                outLayerCurrent[indSet] = layerIn[indSet]
+                OutputObj.output_local[force_idx, :] = outLayerCurrent
+
+                outLayerCurrent_elem = OutputObj.output_local_elem[force_idx,:]
+                layerIn_elem = input_forcings.final_forcings_elem[force_idx,:]
+                indSet_elem = np.where(layerIn_elem != ConfigOptions.globalNdv)
+                outLayerCurrent_elem[indSet_elem] = layerIn_elem[indSet_elem]
+                OutputObj.output_local_elem[force_idx, :] = outLayerCurrent_elem
+                # Reset for next iteration and memory efficiency.
+                indSet = None
+                indSet_elem = None
+            elif(ConfigOptions.grid_type == "hydrofabric"):
+                outLayerCurrent = OutputObj.output_local[force_idx,:]
+                layerIn = input_forcings.final_forcings[force_idx,:]
+                indSet = np.where(layerIn != ConfigOptions.globalNdv)
+                outLayerCurrent[indSet] = layerIn[indSet]
+                OutputObj.output_local[force_idx, :] = outLayerCurrent
+                # Reset for next iteration and memory efficiency.
+                indSet = None
+
+    # MpiConfig.comm.barrier()
+
+
+def layer_supplemental_forcing(OutputObj, supplemental_precip, ConfigOptions, MpiConfig):
+    """
+    Function to layer in supplemental precipitation where we have valid values. Any pixel
+    cells that contain missing values will not be layered in, and background input forcings
+    will be used instead.
+    :param OutputObj:
+    :param supplemental_precip:
+    :param ConfigOptions:
+    :param MpiConfig:
+    :return:
+    """
+
+    if(ConfigOptions.grid_type == "gridded"):
+        indSet = np.where(supplemental_precip.final_supp_precip != ConfigOptions.globalNdv)
+        layerIn = supplemental_precip.final_supp_precip
+        layerOut = OutputObj.output_local[supplemental_precip.output_var_idx, :, :]
+        #TODO: review test layering for ExtAnA calculation to replace FE QPE with MPE RAINRATE
+        #If this isn't sufficient, replace QPE with MPE here:
+        #if supplemental_precip.keyValue == 11:
+        #    ConfigOptions.statusMsg = "Performing ExtAnA calculation"
+        #    err_handler.log_msg(ConfigOptions, MpiConfig)
+        if len(indSet[0]) != 0:
+            layerOut[indSet] = layerIn[indSet]
+        else:
+            # We have all missing data for the supplemental precip for this step.
+            layerOut = layerOut
+        # TODO: test that even does anything...?s
+        OutputObj.output_local[supplemental_precip.output_var_idx, :, :] = layerOut
+    elif(ConfigOptions.grid_type == "unstructured"):
+        indSet = np.where(supplemental_precip.final_supp_precip != ConfigOptions.globalNdv)
+        layerIn = supplemental_precip.final_supp_precip
+        layerOut = OutputObj.output_local[supplemental_precip.output_var_idx, :]
+  
+        if len(indSet[0]) != 0:
+            layerOut[indSet] = layerIn[indSet]
+        else:
+            # We have all missing data for the supplemental precip for this step.
+            layerOut = layerOut
+        # TODO: test that even does anything...?s
+        OutputObj.output_local[supplemental_precip.output_var_idx, :] = layerOut
+
+        indSet_elem = np.where(supplemental_precip.final_supp_precip_elem != ConfigOptions.globalNdv)
+        layerIn_elem = supplemental_precip.final_supp_precip_elem
+        layerOut_elem = OutputObj.output_local_elem[supplemental_precip.output_var_idx, :]
+
+        if len(indSet_elem[0]) != 0:
+            layerOut_elem[indSet_elem] = layerIn_elem[indSet_elem]
+        else:
+            # We have all missing data for the supplemental precip for this step.
+            layerOut_elem = layerOut_elem
+        # TODO: test that even does anything...?s
+        OutputObj.output_local_elem[supplemental_precip.output_var_idx, :] = layerOut_elem
+    elif(ConfigOptions.grid_type == "hydrofabric"):
+        indSet = np.where(supplemental_precip.final_supp_precip != ConfigOptions.globalNdv)
+        layerIn = supplemental_precip.final_supp_precip
+        layerOut = OutputObj.output_local[supplemental_precip.output_var_idx, :]
+
+        if len(indSet[0]) != 0:
+            layerOut[indSet] = layerIn[indSet]
+        else:
+            # We have all missing data for the supplemental precip for this step.
+            layerOut = layerOut
+        # TODO: test that even does anything...?s
+        OutputObj.output_local[supplemental_precip.output_var_idx, :] = layerOut
+
