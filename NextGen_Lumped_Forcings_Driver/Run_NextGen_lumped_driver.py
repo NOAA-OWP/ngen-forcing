aeee6df3e28474413804b6a7350fc54a5f7280cb
Jason Ducker <81377226+jduckerOWP@users.noreply.github.com>
2023-11-08 08:52:54 -0500

    Update Run_NextGen_lumped_driver.py
    Update the main Python script for running the NextGen forcings engine that will account for the new Hydrofabric v2 data that includes the hydrofabric parquet file option that will be needed for the forcings metadata to implement NCAR bias calibration and downscaling options.

--- NextGen_Lumped_Forcings_Driver/Run_NextGen_lumped_driver.py
+++ NextGen_Lumped_Forcings_Driver/Run_NextGen_lumped_driver.py
@@ -5,29 +5,29 @@ import time
 start_time = time.time()
 
 ###################### Python executable sample code for AORC Forcings for hydrofabric subset #############################
-NextGen_lumped_forcings_driver("./output",start_time="2023-01-05 00:00:00", end_time="2023-01-06 00:00:00", met_dataset="AORC",hyfabfile="./nextgen_01.gpkg", met_dataset_pathway="./AORC_Forcings/",weights_file=None,netcdf=True,csv=True,bias_calibration=False,downscaling=False,CONUS=False,AnA=False,num_processes=5)
+NextGen_lumped_forcings_driver("./output",start_time="2010-01-05 00:00:00", end_time="2010-01-06 00:00:00", met_dataset="AORC",hyfabfile="./nextgen_01.gpkg", hyfabfile_parquet=None, met_dataset_pathway="./AORC_Forcings/",weights_file=None,netcdf=True,csv=True,bias_calibration=False,downscaling=False,CONUS=False,AnA=False,num_processes=5)
 
 ###################### Python executable sample code for AORC Forcings for CONUS hydrofabric #############################
-NextGen_lumped_forcings_driver("./output",start_time="2023-01-05 00:00:00", end_time="2023-01-06 00:00:00", met_dataset="AORC",hyfabfile="./conus.gpkg", met_dataset_pathway="./AORC_Forcings/",weights_file=None,netcdf=True,csv=False,bias_calibration=False,downscaling=False,CONUS=True,AnA=False,num_processes=5)
+NextGen_lumped_forcings_driver("./output",start_time="2010-01-05 00:00:00", end_time="2010-01-06 00:00:00", met_dataset="AORC",hyfabfile="./conus.gpkg", hyfabfile_parquet=None, met_dataset_pathway="./AORC_Forcings/",weights_file=None,netcdf=True,csv=False,bias_calibration=False,downscaling=False,CONUS=True,AnA=False,num_processes=5)
 
 ###################### Python executable sample code for AORC Forcings for hydrofabric subset on National Water Center (NWC) servers with no AORC forcing dataset required #############################
-NextGen_lumped_forcings_driver("./output",start_time="2023-01-05 00:00:00", end_time="2023-01-06 00:00:00", met_dataset="AORC",hyfabfile="./nextgen_01.gpkg", met_dataset_pathway=None,netcdf=True,csv=True,bias_calibration=False,downscaling=False,CONUS=False,AnA=False,num_processes=5)
+NextGen_lumped_forcings_driver("./output",start_time="2010-01-05 00:00:00", end_time="2010-01-06 00:00:00", met_dataset="AORC",hyfabfile="./nextgen_01.gpkg", hyfabfile_parquet=None, met_dataset_pathway=None,netcdf=True,csv=True,bias_calibration=False,downscaling=False,CONUS=False,AnA=False,num_processes=5)
 
 
 ###################### Python executable sample code for GFS Forcings Medium range for any hydrofabric geopackage #############################
-NextGen_lumped_forcings_driver("./output",start_time=None, end_time=None, met_dataset="GFS",hyfabfile="./nextgen_01.gpkg", met_dataset_pathway="./GFS_Forecast_00z_Cycle/",weights_file=None,netcdf=True,csv=True,bias_calibration=False,downscaling=False,CONUS=False,AnA=False,num_processes=5)
+NextGen_lumped_forcings_driver("./output",start_time=None, end_time=None, met_dataset="GFS",hyfabfile="./nextgen_01.gpkg", hyfabfile_parquet="./nextgen_01.parquet", met_dataset_pathway="./GFS_Forecast_00z_Cycle/",weights_file=None,netcdf=True,csv=True,bias_calibration=True,downscaling=True,CONUS=False,AnA=False,num_processes=5)
 
 
 ###################### Python executable sample code for CFS Forcings Long range for any hydrofabric geopackage #############################
-NextGen_lumped_forcings_driver("./output",start_time=None, end_time=None, met_dataset="CFS",hyfabfile="./nextgen_01.gpkg", met_dataset_pathway="./CFS_Forecast_00z_Cycle/",weights_file=None,netcdf=True,csv=True,bias_calibration=False,downscaling=False,CONUS=False,AnA=False,num_processes=5)
+NextGen_lumped_forcings_driver("./output",start_time=None, end_time=None, met_dataset="CFS",hyfabfile="./nextgen_01.gpkg", hyfabfile_parquet="./nextgen_01.parquet", met_dataset_pathway="./CFS_Forecast_00z_Cycle/",weights_file=None,netcdf=True,csv=True,bias_calibration=True,downscaling=True,CONUS=False,AnA=False,num_processes=5)
 
 
 ###################### Python executable sample code for HRRR Forcings Short range configuration for any hydrofabric geopackage #############################
-NextGen_lumped_forcings_driver("./output",start_time=None, end_time=None, met_dataset="HRRR",hyfabfile="./nextgen_01.gpkg", met_dataset_pathway="./HRRR_Data_Directory/",weights_file=None,netcdf=True,csv=True,bias_calibration=False,downscaling=False,CONUS=False,AnA=False,num_processes=5)
+NextGen_lumped_forcings_driver("./output",start_time="2010-01-05 00:00:00", end_time=None, met_dataset="HRRR",hyfabfile="./nextgen_01.gpkg", hyfabfile_parquet="./nextgen_01.parquet", met_dataset_pathway="./HRRR_Data_Directory/",weights_file=None,netcdf=True,csv=True,bias_calibration=True,downscaling=True,CONUS=False,AnA=False,num_processes=5)
 # Note, The met_data_pathway="./HRRR_Data_Directory/" data pathway here is just for the pathway to where all the HRRR daily forecast cycles are located (e.g. hrrr.20230104  hrrr.20230105, etc. directoires that are located inside the HRRR_Data_Directory pathway)
 
 ###################### Python executable sample code for HRRR Forcings Analysis and Assimilation (AnA) configuration for any hydrofabric geopackage #############################
-NextGen_lumped_forcings_driver("./output",start_time=None, end_time=None, met_dataset="HRRR",hyfabfile="./nextgen_01.gpkg", met_dataset_pathway="./HRRR_Data_Directory/",weights_file=None,netcdf=True,csv=True,bias_calibration=False,downscaling=False,CONUS=False,AnA=True,num_processes=5)
+NextGen_lumped_forcings_driver("./output",start_time="2010-01-05 00:00:00", end_time=None, met_dataset="HRRR",hyfabfile="./nextgen_01.gpkg", hyfabfile_parquet="./nextgen_01.parquet", met_dataset_pathway="./HRRR_Data_Directory/",weights_file=None,netcdf=True,csv=True,bias_calibration=True,downscaling=True,CONUS=False,AnA=True,num_processes=5)
 # Note, The met_data_pathway="./HRRR_Data_Directory/" data pathway here is just for the pathway to where all the HRRR daily forecast cycles are located (e.g. hrrr.20230104  hrrr.20230105, etc. directoires that are located inside the HRRR_Data_Directory pathway)
 
 
